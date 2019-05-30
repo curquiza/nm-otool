464c5d2a3d0f5411b9c10223c80094413696a9b9
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-05-30 19:04:34 +0200

    init file normalized

--- src/file64_init.c
+++ src/file64_init.c
@@ -8,10 +8,6 @@ static t_ex_ret		get_sections_indexes(t_bin_file *file,
 	struct section_64			*section;
 	uint32_t					seg_nsects;
 
-	// section = (struct section_64 *)((void *)seg + sizeof(*seg)); //check size
-	// section = (struct section_64 *)check_and_move(file, (void *)seg + sizeof(*seg), sizeof(*section));
-	// if (!section)
-	// 	return (ft_ret_err2(file->filename, VALID_OBJ_ERR));
 	seg_nsects = swap_uint32_if(seg->nsects, file->endian);
 	i = 0;
 	while (i < seg_nsects)
@@ -28,10 +24,6 @@ static t_ex_ret		get_sections_indexes(t_bin_file *file,
 		else if (ft_strcmp(section->sectname, SECT_BSS) == 0)
 			file->bss_index = current_sect_index + i;
 		i++;
-		// section = (struct section_64 *) ((void *)section + sizeof(*section)); //check size
-		// section = (struct section_64 *)check_and_move(file, (void *)section + sizeof(*section), sizeof(*section));
-		// if (!section)
-		// 	return (ft_ret_err2(file->filename, VALID_OBJ_ERR));
 	}
 	return (SUCCESS);
 }
@@ -45,14 +37,12 @@ static t_ex_ret		get_info_from_lc(t_bin_file *file, struct load_command *lc,
 	lc_cmd = swap_uint32_if(lc->cmd, file->endian);
 	if (lc_cmd == LC_SYMTAB)
 	{
-		// file->symtab_lc = (struct symtab_command *)lc; //check size
 		file->symtab_lc = (struct symtab_command *)check_and_move(file, lc, sizeof(*lc)); //check size
 		if (!file->symtab_lc)
 			return (ft_ret_err2(file->filename, VALID_OBJ_ERR));
 	}
 	else if (lc_cmd == LC_SEGMENT_64)
 	{
-		// seg = (struct segment_command_64 *)lc;
 		seg = (struct segment_command_64 *)check_and_move(file, lc, sizeof(*seg));
 		if (!seg)
 			return (ft_ret_err2(file->filename, VALID_OBJ_ERR));
@@ -60,6 +50,19 @@ static t_ex_ret		get_info_from_lc(t_bin_file *file, struct load_command *lc,
 			return (FAILURE);
 		*section_index += swap_uint32_if(seg->nsects, file->endian);
 	}
+	return (SUCCESS);
+}
+
+static t_ex_ret		init_header_and_lc(t_bin_file *file,
+						struct mach_header_64 **header,
+						struct load_command	**lc)
+{
+	*header = (struct mach_header_64 *)check_and_move(file, file->ptr,
+		sizeof(**header));
+	*lc = (struct load_command *)check_and_move(file,
+		file->ptr + sizeof(**header), sizeof(**lc));
+	if (!*header || !*lc)
+		return (ft_ret_err2(file->filename, VALID_OBJ_ERR));
 	return (SUCCESS);
 }
 
@@ -71,14 +74,8 @@ static t_ex_ret		get_file_info(t_bin_file *file)
 	struct mach_header_64		*header;
 	uint32_t					header_ncmds;
 
-	// header = (struct mach_header_64 *)file->ptr;
-	header = (struct mach_header_64 *)check_and_move(file, file->ptr,
-		sizeof(*header));
-	// lc = file->ptr + sizeof(*header);
-	lc = (struct load_command *)check_and_move(file,
-		file->ptr + sizeof(*header), sizeof(*lc));
-	if (!header || !lc)
-		return (ft_ret_err2(file->filename, VALID_OBJ_ERR));
+	if (init_header_and_lc(file, &header, &lc) == FAILURE)
+		return (FAILURE);
 	section_index = 1;
 	header_ncmds = swap_uint32_if(header->ncmds, file->endian);
 	i = 0;
@@ -89,11 +86,10 @@ static t_ex_ret		get_file_info(t_bin_file *file)
 		if (get_info_from_lc(file, lc, &section_index) == FAILURE)
 			return (FAILURE);
 		i++;
-		// lc = (void *)lc + lc->cmdsize; //check size)
 		lc = (struct load_command *)check_and_move(file,
 			(void *)lc + swap_uint32_if(lc->cmdsize, file->endian),
-			sizeof(*lc)); //check size
-		if (i < header_ncmds && !lc) // sinon on peut depasser la taille du fichier sans que ce soit important car fin de boucle
+			sizeof(*lc));
+		if (i < header_ncmds && !lc)
 			return (ft_ret_err2(file->filename, VALID_OBJ_ERR));
 	}
 	return (SUCCESS);
