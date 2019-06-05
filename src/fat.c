7431de08c38a9640aa850a7603e684d6d88c79f7
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-06-05 23:34:11 +0200

    fix segfault in fat

--- src/fat.c
+++ src/fat.c
@@ -11,8 +11,6 @@ static char	*get_archi_name(cpu_type_t cpu_type, cpu_subtype_t cpu_subtype)
 		return ("ppc");
 	else if (cpu_type == CPU_TYPE_POWERPC64)
 		return ("ppc64");
-	else if (cpu_type == CPU_TYPE_X86_64)
-		return ("x86_64");
 	else if (cpu_type == CPU_TYPE_ARM64)
 		return ("arm64");
 	else if (cpu_type == CPU_TYPE_ARM  && cpu_subtype == CPU_SUBTYPE_ARM_V7)
@@ -40,9 +38,13 @@ static t_ex_ret	if_same_arch_process(t_bin_file *file)
 	while (arch_nb--)
 	{
 		if (is_archi_x86_64(swap_uint32_if(arch->cputype, file->endian)))
+		{
+			if (!check_and_move(file, file->ptr + swap_uint32_if(arch->offset, file->endian), swap_uint32_if(arch->size, file->endian)))
+				return (ft_ret_err2(file->filename, FILE_END_ERR));
 			return (ft_nm(swap_uint32_if(arch->size, file->endian),
 					file->ptr + swap_uint32_if(arch->offset, file->endian),
 					file->filename));
+		}
 		arch = (struct fat_arch *)check_and_move(file, arch + 1, sizeof(*arch));
 		if (!arch)
 			return (ft_ret_err2(file->filename, FILE_END_ERR));
@@ -58,7 +60,6 @@ static t_ex_ret not_same_arch_process(t_bin_file *file)
 	cpu_type_t			cpu_type;
 	cpu_subtype_t		cpu_subtype;
 
-	// header = (struct fat_header*)file->ptr;
 	if (!(header = (struct fat_header*)check_and_move(file, file->ptr,
 			sizeof(*header))))
 		return (ft_ret_err2(file->filename, VALID_OBJ_ERR));
@@ -73,13 +74,14 @@ static t_ex_ret not_same_arch_process(t_bin_file *file)
 		ft_printf("\n%s (for architecture %s):\n",
 			file->filename, get_archi_name(cpu_type, cpu_subtype));
 
-		if (!check_and_move(file, file->ptr + swap_uint32_if(arch->offset, file->endian), 1))
+		if (!check_and_move(file, file->ptr + swap_uint32_if(arch->offset, file->endian), swap_uint32_if(arch->size, file->endian)))
 			return (ft_ret_err2(file->filename, FILE_END_ERR));
 
 		if (ft_nm(swap_uint32_if(arch->size, file->endian),
 			file->ptr + swap_uint32_if(arch->offset, file->endian),
 			file->filename) == FAILURE)
 			return (FAILURE);
+
 		arch = (struct fat_arch *)check_and_move(file, arch + 1, sizeof(*arch));
 		if (!arch)
 			return (ft_ret_err2(file->filename, FILE_END_ERR));
