d3d750f71b1643933d49cf260f3b0c7eb2b9a9ba
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-06-07 18:32:28 +0200

    norm archive

--- src/archive.c
+++ src/archive.c
@@ -11,17 +11,16 @@ static uint32_t	ft_padding_ar_mac(char *ar_name)
 	return (ar_name_size);
 }
 
-static t_ex_ret	exec_all_objetcs(t_bin_file *file, struct ranlib *symtab, uint32_t symtab_size)
+static t_ex_ret	exec_all_objetcs(t_bin_file *file, struct ranlib *symtab,
+					uint32_t symtab_size, uint32_t string_table_size)
 {
 	struct ar_hdr		*obj_header;
 	char				*obj_name;
 	uint32_t			obj_size;
-	uint32_t			string_table_size;
 
-	if (check_and_move(file, (void*)symtab + symtab_size, sizeof(string_table_size)) == NULL)
-		return (ft_ret_err2(file->filename, FILE_END_ERR));
-	string_table_size = *(uint32_t*)((void*)symtab + symtab_size);
-	obj_header = check_and_move(file, (void*)symtab + symtab_size + sizeof(uint32_t) + string_table_size, sizeof(*obj_header));
+	obj_header = check_and_move(file, (void*)symtab
+		+ symtab_size + sizeof(uint32_t)
+		+ string_table_size, sizeof(*obj_header));
 	while (obj_header)
 	{
 		if (check_and_move(file, obj_header + 1, sizeof(*obj_name)) == NULL)
@@ -30,35 +29,41 @@ static t_ex_ret	exec_all_objetcs(t_bin_file *file, struct ranlib *symtab, uint32
 		obj_size = ft_atoi(obj_header->ar_size);
 		if (check_and_move(file, (void *)obj_name, obj_size) == NULL)
 			return (ft_ret_err2(file->filename, FILE_END_ERR));
-		if (ft_nm(obj_size, (void *)obj_name + ft_padding_ar_mac(obj_header->ar_name), obj_name, file->filename) == FAILURE)
+		if (ft_nm(obj_size, (void *)obj_name
+				+ ft_padding_ar_mac(obj_header->ar_name),
+				obj_name, file->filename) == FAILURE)
 			return (FAILURE);
-		obj_header = check_and_move(file, (void *)obj_header + sizeof(*obj_header) + obj_size, sizeof(*obj_header));
+		obj_header = check_and_move(file, (void *)obj_header
+			+ sizeof(*obj_header) + obj_size, sizeof(*obj_header));
 	}
 	return (SUCCESS);
 }
 
-t_ex_ret	 handle_archive(char *filename, uint64_t size, void *ptr)
+t_ex_ret		handle_archive(char *filename, uint64_t size, void *ptr)
 {
 	struct ar_hdr		*header;
 	struct ranlib		*symtab;
 	uint32_t			symtab_size;
 	t_bin_file			file;
+	uint32_t			string_table_size;
 
 	file.filename = filename;
 	file.size = size;
 	file.ptr = ptr;
-	// header = (struct ar_hdr*)(ptr + SARMAG);
-	header = (struct ar_hdr*)check_and_move(&file, ptr + SARMAG, sizeof(*header));
-	if (!header)
-		// return (ft_ret_err2(filename, FILE_END_ERR));
+	if (!(header = (struct ar_hdr*)check_and_move(&file, ptr + SARMAG,
+		sizeof(*header))))
 		return (SUCCESS);
-	symtab_size = *(uint32_t*)((void*)(header + 1) + ft_padding_ar_mac(header->ar_name));
-	// symtab_size = *(uint32_t*)((void*)(header + 1) + ft_padding_ar_mac(header->ar_name));
+	symtab_size = *(uint32_t*)((void*)(header + 1)
+		+ ft_padding_ar_mac(header->ar_name));
 	if (check_and_move(&file, header + 1, sizeof(symtab_size)) == NULL)
 		return (ft_ret_err2(filename, FILE_END_ERR));
-	// symtab = (struct ranlib*) ((void*)(header + 1) + ft_padding_ar_mac(header->ar_name) + sizeof(uint32_t));
-	symtab = (struct ranlib*)check_and_move(&file, (void*)(header + 1) + ft_padding_ar_mac(header->ar_name) + sizeof(uint32_t), sizeof(*symtab));
-	if (symtab == NULL)
-		return (ft_ret_err2(filename, FILE_END_ERR));
-	return (exec_all_objetcs(&file, symtab, symtab_size));
+	symtab = (struct ranlib*)check_and_move(&file, (void*)(header + 1)
+		+ ft_padding_ar_mac(header->ar_name)
+		+ sizeof(uint32_t), sizeof(*symtab));
+	if (symtab == NULL
+		|| check_and_move(&file, (void*)symtab + symtab_size,
+			sizeof(string_table_size)) == NULL)
+		return (ft_ret_err2(file.filename, FILE_END_ERR));
+	string_table_size = *(uint32_t*)((void*)symtab + symtab_size);
+	return (exec_all_objetcs(&file, symtab, symtab_size, string_table_size));
 }
