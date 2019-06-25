8c86a51c5f1327de570e3e6a3f5db9787570a4f1
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-06-25 13:26:00 +0200

    norm for otool done

--- otool/src/ft_otool.c
+++ otool/src/ft_otool.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_otool.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: curquiza <curquiza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/25 13:24:09 by curquiza          #+#    #+#             */
+/*   Updated: 2019/06/25 13:24:10 by curquiza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_otool.h"
 
 static void	display_name(char *filename, char *archive_name)
@@ -8,60 +20,32 @@ static void	display_name(char *filename, char *archive_name)
 		ft_printf("%s(%s):\n", archive_name, filename);
 }
 
-t_ex_ret	ft_otool(uint64_t size, void *ptr, char *filename, char *archive_name)
+t_ex_ret	ft_otool(uint64_t size, void *ptr, char *filename,
+				char *archive_name)
 {
 	uint32_t	magic_number;
 
 	display_name(filename, archive_name);
 	magic_number = *(uint32_t *)ptr;
 	if (magic_number == MH_MAGIC)
-	{
-		// ft_printf("MAGIC 32 bits\n");
 		return (handle_32(size, ptr, filename, MAGIC));
-	}
 	else if (magic_number == MH_MAGIC_64)
-	{
-		// ft_printf("MAGIC 64 bits\n");
 		return (handle_64(size, ptr, filename, MAGIC));
-	}
 	else if (magic_number == MH_CIGAM)
-	{
-		// ft_printf("CIGAM 32 bits\n");
 		return (handle_32(size, ptr, filename, CIGAM));
-	}
 	else if (magic_number == MH_CIGAM_64)
-	{
-		// ft_printf("CIGAM 64 bits\n");
 		return (handle_64(size, ptr, filename, CIGAM));
-	}
 	else if (magic_number == FAT_MAGIC)
-	{
-		// ft_printf("FAT_MAGIC 32\n");
 		return (handle_fat32(filename, size, ptr, MAGIC));
-	}
 	else if (magic_number == FAT_MAGIC_64)
-	{
 		return (handle_fat64(filename, size, ptr, MAGIC));
-		// ft_printf("FAT_MAGIC 64\n");
-	}
 	else if (magic_number == FAT_CIGAM)
-	{
-		// ft_printf("FAT_CIGAM 32\n");
 		return (handle_fat32(filename, size, ptr, CIGAM));
-	}
 	else if (magic_number == FAT_CIGAM_64)
-	{
-		// ft_printf("FAT_CIGAM 64\n");
 		return (handle_fat64(filename, size, ptr, CIGAM));
-	}
 	else if (ft_strncmp((char *)ptr, ARMAG, SARMAG) == 0)
-	{
-		// ft_printf("ARCHIVE\n");
 		return (handle_archive(filename, size, ptr));
-	}
 	else
-	{
 		ft_dprintf(2, "Error: Unknown magic number : %x\n", magic_number);
-	}
 	return (FAILURE);
 }
--- otool/src/print_output.c
+++ otool/src/print_output.c
@@ -1,43 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_output.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: curquiza <curquiza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/25 13:24:25 by curquiza          #+#    #+#             */
+/*   Updated: 2019/06/25 13:24:26 by curquiza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_otool.h"
-
-// static t_ex_ret	print_compacted(t_bin_file *file, size_t i)
-// {
-// 	size_t	cpt;
-// 	void	*offset;
-
-// 	cpt = 0;
-// 	while (cpt < 16)
-// 	{
-// 		offset = (void *)file->ptr + file->text_offset + i + cpt;
-// 		if (check_and_move(file, offset, sizeof(uint32_t)) == NULL)
-// 			return (ft_ret_err2(file->filename, MALF_OBJ_ERR));
-// 		if (offset >= (void *)file->ptr + file->text_offset + file->text_size)
-// 			return (SUCCESS);
-// 		ft_printf("%-9.8x", swap_uint32_if(*(uint32_t *)offset, file->endian));
-// 		cpt += 4;
-// 	}
-// 	return (SUCCESS);
-// }
-
-// static t_ex_ret	print_spaced(t_bin_file *file, size_t i)
-// {
-// 	size_t	cpt;
-// 	void	*offset;
-
-// 	cpt = 0;
-// 	while (cpt < 16)
-// 	{
-// 		offset = (void *)file->ptr + file->text_offset + i + cpt;
-// 		if (check_and_move(file, offset, sizeof(uint8_t)) == NULL)
-// 			return (ft_ret_err2(file->filename, MALF_OBJ_ERR));
-// 		if (offset >= (void *)file->ptr + file->text_offset + file->text_size)
-// 			return (SUCCESS);
-// 		ft_printf("%-3.2hhx", (uint8_t)swap_uint32_if(*(uint32_t *)offset,
-// 			file->endian));
-// 		cpt++;
-// 	}
-// 	return (SUCCESS);
-// }
 
 static void		print_compacted(t_bin_file *file, size_t i)
 {
@@ -71,22 +44,6 @@ static void		print_spaced(t_bin_file *file, size_t i)
 		cpt++;
 	}
 }
-
-// static t_ex_ret	check_malformed_obj(t_bin_file *file)
-// {
-// 	size_t	i;
-// 	void	*offset;
-
-// 	i = 0;
-// 	while (i < file->text_size)
-// 	{
-// 		offset = (void *)file->ptr + file->text_offset + i + 15;
-// 		if (check_and_move(file, offset, sizeof(uint8_t)) == NULL)
-// 			return (ft_ret_err2(file->filename, MALF_OBJ_ERR));
-// 		i += 16;
-// 	}
-// 	return (SUCCESS);
-// }
 
 static t_ex_ret	check_malformed_obj(t_bin_file *file)
 {
