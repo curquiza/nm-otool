81f9ab8415f39c68c0c44bc493e32b158c0e019f
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-06-27 13:57:25 +0200

    add all tests

--- tests/input_cmds.py
+++ tests/input_cmds.py
@@ -6,13 +6,11 @@ INPUT_CMDS = [
     { 'cmd': 'nm/ft_nm tests/input/64_exe_medium',                   'error': False,  'output': '64_exe_medium.output' },
     { 'cmd': 'nm/ft_nm tests/input/64_lib_dynamic_hard',             'error': False,  'output': '64_lib_dynamic_hard.output' },
     { 'cmd': 'nm/ft_nm tests/input/64_lib_dynamic_medium',           'error': False,  'output': '64_lib_dynamic_medium.output' },
-    # { 'cmd': 'nm/ft_nm tests/input/MachO-OSX-ppc-and-i386-bash',     'error': False,  'output': 'MachO-OSX-ppc-and-i386-bash.output'},
     { 'cmd': 'nm/ft_nm tests/input/MachO-OSX-x64-ls',                'error': False,  'output': 'MachO-OSX-x64-ls.output' },
     { 'cmd': 'nm/ft_nm tests/input/MachO-OSX-x86-ls',                'error': False,  'output': 'MachO-OSX-x86-ls.output' },
     { 'cmd': 'nm/ft_nm tests/input/MachO-iOS-arm1176JZFS-bash',      'error': False,  'output': 'MachO-iOS-arm1176JZFS-bash.output' },
     { 'cmd': 'nm/ft_nm tests/input/MachO-iOS-armv7s-Helloworld',     'error': False,  'output': 'MachO-iOS-armv7s-Helloworld.output' },
     { 'cmd': 'nm/ft_nm tests/input/fat_hard',                        'error': False,  'output': 'fat_hard.output' },
-    # { 'cmd': 'nm/ft_nm tests/input/lib_archive_medium',              'error': False,  'output': 'lib_archive_medium.output' },
     { 'cmd': 'nm/ft_nm tests/input/object_easy',                     'error': False,  'output': 'object_easy.output' },
     { 'cmd': 'nm/ft_nm tests/input/object_medium',                   'error': False,  'output': 'object_medium.output' },
     { 'cmd': 'nm/ft_nm tests/input/test_32',                         'error': False,  'output': 'test_32.output' },
@@ -30,8 +28,19 @@ INPUT_CMDS = [
     { 'cmd': 'nm/ft_nm -n -r tests/input/64_bundle',                 'error': False, 'output': 'multi_opt_1.output' },
     { 'cmd': 'nm/ft_nm -gn tests/input/basic_64',                    'error': False, 'output': 'multi_opt_2.output' },
     { 'cmd': 'nm/ft_nm -jgn tests/input/basic_64',                   'error': False, 'output': 'multi_opt_3.output' },
+    { 'cmd': 'nm/ft_nm -n tests/input/64_bundle -r',                 'error': True,  'output': 'multi_opt_4.output' },
+    { 'cmd': 'nm/ft_nm -n tests/input/64_bundle',                    'error': False, 'output': 'multi_opt_5.output' },
+    { 'cmd': 'nm/ft_nm -uU tests/input/test_32',                     'error': False, 'output': 'multi_opt_6.output' },
+    { 'cmd': 'nm/ft_nm -Uujg tests/input/test_32',                   'error': False, 'output': 'multi_opt_7.output' },
+    { 'cmd': 'nm/ft_nm -jg -r tests/input/test_32',                  'error': False, 'output': 'multi_opt_8.output' },
+    { 'cmd': 'nm/ft_nm -g -nr tests/input/test_32',                  'error': False, 'output': 'multi_opt_9.output' },
+    { 'cmd': 'nm/ft_nm -jgn tests/input/basic_64',                   'error': False, 'output': 'multi_opt_10.output' },
+    { 'cmd': 'nm/ft_nm -ujgn tests/input/basic_64',                  'error': False, 'output': 'multi_opt_11.output' },
+    { 'cmd': 'nm/ft_nm -Ujnp tests/input/basic_64',                  'error': False, 'output': 'multi_opt_12.output' },
+    { 'cmd': 'nm/ft_nm -ugp tests/input/basic_64',                   'error': False, 'output': 'multi_opt_13.output' },
+
+    { 'cmd': 'nm/ft_nm -ugp tests/input/basic_64 tests/input/object_medium lol tests/input/basic_64',          'error': True,  'output': 'multi_file_1.output' },
+    { 'cmd': 'nm/ft_nm -jgpr tests/input/basic_64 tests/input/object_medium tests/input/basic_64',             'error': False, 'output': 'multi_file_2.output' },
+    { 'cmd': 'nm/ft_nm -pU tests/input/basic_64 tests/input/object_medium tests/input/basic_64',               'error': False, 'output': 'multi_file_3.output' },
+    { 'cmd': 'nm/ft_nm -rjU tests/input/basic_64 tests/input/object_medium tests/input/basic_64',              'error': False, 'output': 'multi_file_4.output' },
 ]
-
-
-# test multi fichier
-# tests options mixees
